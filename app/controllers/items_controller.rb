10f24f6007499222c088d7924c39e2917931c256
EC2 Default User <ec2-user@ip-172-31-82-147.ec2.internal>
2022-06-26 16:23:25 +0000

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,15 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
+
+private
+def item_params
+  params.require(:item).permit(:title, :body, :image)
+end
+
